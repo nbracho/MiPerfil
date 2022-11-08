ab3e1e0c96ab53bd5f292f65148ddfeef0523f74
unknown <nicolasbracho@gmail.com>
2022-11-08 15:33:52 -0400

    Correccion Lorep Ipsum

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -69,8 +69,7 @@ function encode(data) {
             ¡Contrátame!
           </h2>
           <p className="leading-relaxed mb-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            suscipit officia aspernatur veritatis. Asperiores, aliquid?
+Envia una Solicitud de Contacto
           </p>
           <div className="relative mb-4">
             <label htmlFor="name" className="leading-7 text-sm text-gray-400">
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,9 +13,7 @@ export default function Skills() {
             Habilidades y Tecnologías
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+Estoy Relacionado con Todas estas Tecnologias en Niveles de Intermedio a Avanzado.
           </p>
         </div>
         <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
