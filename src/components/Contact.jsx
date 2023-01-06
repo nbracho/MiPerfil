caf1351b1530d77fe112294cd9499a8ae2129eb0
unknown <nicolasbracho@gmail.com>
2023-01-06 19:34:13 -0400

    actualizacion nbasoft ws final

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -27,7 +27,7 @@ function encode(data) {
   }
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap" style="width:100% !important;">
         <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
           <iframe
             width="100%"
@@ -40,7 +40,7 @@ function encode(data) {
             style={{ filter: "opacity(0.7)" }}
             src="https://www.google.com/maps/embed/v1/place?q=C.C+La+Fuente,+Cabimas,+Zulia,+Venezuela&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"
           />
-          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md" style="width: 53%;">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 DIRECCIÓN
